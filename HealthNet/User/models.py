3ce448f21518c2476d612d26cf3a6331e92ed509
jxr1210 <jxr1210@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-09-28 23:23:03 +0000

    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@22 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/User/models.py
+++ HealthNet/User/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from Calendar.models import Calendar
 
 # Create your models here.
+
+class User(models.Model):
+
+    Calendar = models.ForeignKey(Calendar , on_delete=models.CASCADE)
+    
+    #Notification = models.ForeignKey(Notification , on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
