6aa560442a4d12d092dd371eb1f1359e6d887110
jxr1210 <jxr1210@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-09-28 23:33:13 +0000

    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@24 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/User/models.py
+++ HealthNet/User/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from Calendar.models import Calendar
+from Hospital.models import Hospital
 
 # Create your models here.
 
 class User(models.Model):
 
     Calendar = models.ForeignKey(Calendar , on_delete=models.CASCADE)
-    
+    UserName = models.CharField(max_length=15)
+    Password = models.CharField(max_length=20)
+    Hospital = models.ForeignKey(Hospital , on_delete=models.CASCADE)
     #Notification = models.ForeignKey(Notification , on_delete=models.CASCADE)
 
     class Meta:
--- HealthNet/hospital/models.py
+++ HealthNet/hospital/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 # Create your models here.
 
+class Hospital(models.Model):
+    name = models.CharField
