90ef49260f523c76e6c08c5b11eb37193dd2b98d
jmk1154 <jmk1154@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-09-28 23:24:01 +0000

    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@23 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/logIn/tests.py
+++ HealthNet/logIn/tests.py
@@ -1,29 +1,29 @@
 from django.test import TestCase
 
-def RegistrationTests(TestCase):
+class RegistrationTests(TestCase):
 
-    def test_register_a_patient_by_form():
+    def test_register_a_patient_by_form(self):
         """Attempt To register a user and make sure they show up in the database"""
 
-    def test_register_a_patient_twice():
+    def test_register_a_patient_twice(self):
         """Attempt to register a patient twice, make sure the appropriate message is displayed"""
-        #TODO: determine what message is returned
+        # TODO: determine what message is returned
 
-    def test_invalid_insurace_numer():
+    def test_invalid_insurace_numer(self):
         """Attept to register a user with an invalid insurance Number, make sure it redirects to health insurance website"""
 
-def LoginTests(TestCase):
+class LoginTests(TestCase):
 
-    def test_login_form_patient():
+    def test_login_form_patient(self):
         """Create a new patient and attempt to login as that user"""
 
-    def test_login_form_nurse():
+    def test_login_form_nurse(self):
         """Create a new nurse and attempt to login"""
 
-    def test_login_form_doctor():
+    def test_login_form_doctor(self):
         """Create a new Doctor and attempt to login as doctor"""
 
-    def attempt_to_login_with_fake_user():
+    def attempt_to_login_with_fake_user(self):
         """Attempt to login with fake user and make sure it redirects to approprate page"""
-        #TODO: figure out what page it would redrect to
+        # TODO: figure out what page it would redrect to
 
