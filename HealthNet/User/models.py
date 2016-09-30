1c862e71061a785ffdc9224e7bae88e2c5e10732
crw4147 <crw4147@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-09-30 00:27:14 +0000

    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@29 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/User/models.py
+++ HealthNet/User/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
+#from Calendar.models import Calendar
+#from hospital.models import Hospital
+#from EMR.models import EMR
+
+#from Hospital.models import Hospital
 from Calendar.models import Calendar, Notification
 from hospital.models import Hospital
+from EMR.models import EMR
 
 
 # Create your models here.
-
 class User(models.Model):
+
 
     Calendar = models.ForeignKey(Calendar , on_delete=models.CASCADE)
     UserName = models.CharField(max_length=15)
@@ -14,5 +20,14 @@ class User(models.Model):
     Hospital = models.ForeignKey(Hospital , on_delete=models.CASCADE)
     Notification = models.ForeignKey(Notification , on_delete=models.CASCADE)
 
+
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return str(self.name)
+
+# this extension of User represents a patient
+class Patient(User):
+    insuranceNum = models.CharField(max_length=12)
+    EMR = models.ForeignKey(EMR, on_delete=models.CASCADE)
