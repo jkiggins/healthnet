688240934ee5b74982ae677476da0c2198801d89
jmk1154 <jmk1154@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-10-01 14:38:58 +0000

    added syslogging app and created Syslog Model
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@32 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/User/models.py
+++ HealthNet/User/models.py
@@ -8,10 +8,10 @@ from emr.models import EMR
 # Create your models here.
 class User(models.Model):
 
-    Calendar = models.OneToOneField(Calendar)
+    Calendar = models.OneToOneField(Calendar, null=True, blank=True)
     UserName = models.CharField(max_length=15, default="")
     Password = models.CharField(max_length=20, default="")
-    Notification = models.ManyToManyField(Notification)
+    Notification = models.ManyToManyField(Notification, null=True, blank=True)
 
     firstName = models.CharField(max_length=20, default="")
     lastName = models.CharField(max_length=20, default="")
@@ -26,8 +26,8 @@ class User(models.Model):
 
 # this extension of User represents a patient
 class Patient(User):
-    hospital = models.OneToOneField(Hospital, null=True, blank=True)
-    doctor = models.ForeignKey('Doctor', on_delete=models.CASCADE, null=True, blank=True)
+    hospital = models.ForeignKey(Hospital, null=True, blank=True)
+    doctor = models.ForeignKey('Doctor', null=True, blank=True)
     insuranceNum = models.CharField(max_length=12, default="")
     emr = models.OneToOneField(EMR, null=True, blank=True)
 
--- HealthNet/hospital/models.py
+++ HealthNet/hospital/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Hospital(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default="")
 
     def __str__(self):
         return str(self.name)
