46eb86354d34d3f298c6a2c11e66d20b2067a791
crw4147 <crw4147@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-10-02 00:59:02 +0000

    updated basic urls.py
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@60 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/User/urls.py
+++ HealthNet/User/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'User'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-
+    url(r'^$', views.patientList, name='pList'),
+    url(r'^$', views.viewProfile, name='vProfile'),
+    url(r'^$', views.eventCreate, name='eCreate'),
 ]
