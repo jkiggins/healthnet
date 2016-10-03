be13583bfeb5d0c11d6428820c7f062992481baf
dsm1252 <dsm1252@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-10-03 04:41:57 +0000

    Added some fields to user registration form
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@83 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/logIn/forms.py
+++ HealthNet/logIn/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-# from User.models import *
+from User.models import *
 from django.contrib.auth.models import User
 
 
@@ -7,21 +7,17 @@ from django.contrib.auth.models import User
 class RegistrationForm(forms.Form):
 
     username = forms.CharField(max_length=30)
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
     email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label="Email address")
     password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label="Password")
     password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label="Password (again)")
 
 
-
-
-#
-# class PatientForm(forms.ModelForm):
-#     class Meta:
-#         model = Patient
-#         #from User class
-#         UserName = forms.CharField()
-#         Password = forms.CharField()
-#         # from patient class
-#         insuranceNum = forms.CharField() # TODO: Check valid
-#         fields = ['UserName', 'Password', 'insuranceNum']
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        # from patient class
+        insuranceNum = forms.CharField() # TODO: Check valid
+        fields = ['insuranceNum']
 
