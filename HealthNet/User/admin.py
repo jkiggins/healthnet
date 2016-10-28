05a7b39bd834dfc5cdbe0d9fcc1cd3e5ab078b70
jmk1154 <jmk1154@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-10-28 02:12:18 +0000

    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@133 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/User/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from User.models import *
-
-# Register your models here.
-admin.site.register(Patient)
-admin.site.register(Nurse)
-admin.site.register(Doctor)
-admin.site.register(Event)
