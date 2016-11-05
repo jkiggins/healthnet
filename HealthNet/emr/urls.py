dd29ff1d4158d906f6594ceb57f34e7118a8a8d2
jmk1154 <jmk1154@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-11-05 16:47:50 +0000

    changed emr urls
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@152 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/emr/urls.py
+++ HealthNet/emr/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'emr'
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/', views.index, name='index'),
+    url(r'^/', views.viewSelfEMR, name='vsemr'),
+    url(r'^(?P<pk>[0-9]+)/', views.viewEMR, name='vemr'),
+    url(r'^(?P<pk>[0-9]+)/viewItem', views.viewEMRItem, name='vemrItem'),
 ]
