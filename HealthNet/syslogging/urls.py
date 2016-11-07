0b0486452badc1eca41d9e5d6bc9820ef1baeb6c
dsm1252 <dsm1252@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-11-07 23:09:07 +0000

    Stubbed out the view system log url and views
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@197 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/syslogging/urls.py
+++ HealthNet/syslogging/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = 'syslogging'
 urlpatterns = [
-    url(r'^$', views.viewSelfEmr, name='viewlog')
-
+    url(r'^$', views.view_log.as_view(), name='viewlog')
 ]
--- HealthNet/syslogging/views.py
+++ HealthNet/syslogging/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+
+class view_log(View):
+
+    def get(self, request, **kwargs):
+        return HttpResponseRedirect(reverse('user:dashboard'))
+
+    def post(self, request, **kwargs):
+        return HttpResponseRedirect(reverse('user:dashboard'))
