9435140698b0a92a29b793e8df046df174665ae9
dsm1252 <dsm1252@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-11-08 00:03:11 +0000

    Got syslogging Views working Added templates for the page that will display the system log
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@200 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/syslogging/views.py
+++ HealthNet/syslogging/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from .models import Syslog
+from user.viewhelper import get_user
 
 # Create your views here.
 
 class view_log(View):
 
-    def get(self, request, **kwargs):
-        return HttpResponseRedirect(reverse('user:dashboard'))
+    def get(self, request):
+        context = {'system_log': Syslog.objects.all(),
+                   'cuser': get_user(request)}
+        return render(request, 'syslogging/System_Log.html', context)
 
-    def post(self, request, **kwargs):
+    def post(self, request):
         return HttpResponseRedirect(reverse('user:dashboard'))
