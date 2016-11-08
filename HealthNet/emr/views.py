458f526d42f585c26f2c66cd53e409a63dae949d
crw4147 <crw4147@bd0dbc1a-75c9-11e6-a404-1391f2854df0>
2016-11-08 05:20:33 +0000

    son't have to install stuff
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/f261-04a/svn/trunk@213 bd0dbc1a-75c9-11e6-a404-1391f2854df0

--- HealthNet/emr/views.py
+++ HealthNet/emr/views.py
@@ -13,24 +13,6 @@ from user.viewhelper import add_dict_to_model
 
 from reportlab.pdfgen import canvas
 
-def exportPdf(request):
-    cuser = get_user(request)
-    # Create the HttpResponse object with the appropriate PDF headers.
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="EMR.pdf"'
-
-    # Create the PDF object, using the response object as its "file."
-    p = canvas.Canvas(response)
-
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world.")
-
-    # Close the PDF object cleanly, and we're done.
-    p.showPage()
-    p.save()
-    return response
-
 def viewSelfEmr(request):
     cuser = get_user(request)
     if cuser is None:
